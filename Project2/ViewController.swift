f78aac5d768ef6f3e8e79e0cbf6b7727a51d30a7
alekhyabol <alekhya.batchu@lji.io>
2024-06-12 21:49:17 +0530

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Batchu Lakshmi Alekhya on 12/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
